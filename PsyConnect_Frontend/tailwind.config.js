8a5992085d832d062f49507a3a5903b0e0547af7
Juan <juan.herreravane@gmail.com>
2025-11-21 15:44:45 -0500

    Correccion de tailwind, index y .env

--- PsyConnect_Frontend/tailwind.config.js
+++ PsyConnect_Frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./index.html",
@@ -6,8 +7,17 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: '#3b82f6',
-                secondary: '#8b5cf6',
+                primary: {
+                    50: '#eff6ff',
+                    100: '#dbeafe',
+                    500: '#3b82f6',
+                    600: '#2563eb',
+                    700: '#1d4ed8',
+                },
+                secondary: {
+                    500: '#8b5cf6',
+                    600: '#7c3aed',
+                }
             }
         },
     },
